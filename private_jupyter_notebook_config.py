d1c70a2a511da0cf8868f9244439e0877023396f
Ilija Vukotic <ivukotic@cern.ch>
2022-07-19 11:00:09 -0500

    no pswd change

--- private_jupyter_notebook_config.py
+++ private_jupyter_notebook_config.py
@@ -1,4 +1,5 @@
 c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.port = 9999
-c.ServerApp.root_dir = '/workspace/'
+# c.ServerApp.root_dir = '/workspace/'
+c.ServerApp.allow_password_change = False
 c.NotebookApp.open_browser = False
